b0cf5c91e5ae9d6974063538c9b319a29f7f89ab
gustavneustadt <gust@vneustadt.de>
2024-12-29 16:35:41 +0100

    Initial Commit

---
+++ BadgeLedApp/BadgeLedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgeLedAppApp.swift
+//  BadgeLedApp
+//
+//  Created by Gustav on 29.12.24.
+//
+
+import SwiftUI
+
+@main
+struct BadgeLedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
